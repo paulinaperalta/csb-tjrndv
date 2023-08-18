487b99831cc563312aab0615fcd2ae340fbf0686
Code Sandbox <hello@codesandbox.io>
2023-08-18 19:42:01 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.bdc9401a4ff50b76190b59eda3d2de0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83c428e7ff218aa960746a90ae76b52a",
+    "url": "https://paulinaperalta.github.io/csb-tjrndv/index.html"
+  },
+  {
+    "revision": "5420f898ed8575601d1b",
+    "url": "https://paulinaperalta.github.io/csb-tjrndv/static/css/main.3cddf896.chunk.css"
+  },
+  {
+    "revision": "029833c7f783678101ed",
+    "url": "https://paulinaperalta.github.io/csb-tjrndv/static/js/2.743cc59c.chunk.js"
+  },
+  {
+    "revision": "5420f898ed8575601d1b",
+    "url": "https://paulinaperalta.github.io/csb-tjrndv/static/js/main.8d0d246d.chunk.js"
+  },
+  {
+    "revision": "dda82524c29bc875a25f",
+    "url": "https://paulinaperalta.github.io/csb-tjrndv/static/js/runtime~main.b39ef01f.js"
+  },
+  {
+    "revision": "b13026414e4927f4ae53853fad6bfa85",
+    "url": "https://paulinaperalta.github.io/csb-tjrndv/static/media/LatinasCodeCampLogo.b1302641.png"
+  }
+]);
